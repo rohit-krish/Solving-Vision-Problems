6ed06e65529fc847544d14cff4edbcb79f23ffbe
Rohit <krish.rohitnp@gmail.com>
2023-10-03 12:31:40 +0530

    tried to use harris corner_detector; but wasn't satisfactory

--- O-rings/main.py
+++ O-rings/main.py
@@ -19,6 +19,30 @@ def get_clean_contour(thresh):
     return thresh, contours
 
 
+def harris_corner_detector(gray, blockSize=3, ksize=3, k=0.05, threshold=0.04, sigma=0):
+    """
+    gray - Input image, it should be grayscale and float32 type.
+    blockSize - It is the size of neighbourhood considered for corner detection
+    ksize - Aperture parameter of Sobel derivative used.
+    k - Harris detector free parameter in the equation.
+    sigma - stddev value for the guassian blur operation
+    """
+
+    Ix = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=ksize)
+    Iy = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=ksize)
+
+    Ix2 = cv2.GaussianBlur(Ix**2, (blockSize, blockSize), sigma)
+    Iy2 = cv2.GaussianBlur(Iy**2, (blockSize, blockSize), sigma)
+    Ixy = cv2.GaussianBlur(Ix * Iy, (blockSize, blockSize), sigma)
+
+    det_M = (Ix2 * Iy2) - (Ixy**2)
+    trace_M = Ix2 + Iy2
+    cornerness = det_M - (k * (trace_M**2))
+
+    corners = np.where(cornerness > threshold * cornerness.max())
+    return list(zip(corners[1], corners[0]))
+
+
 for img_name in listdir("./assets/"):
     img = cv2.imread("./assets/" + img_name)
     h, w, _ = img.shape
@@ -36,20 +60,13 @@ for img_name in listdir("./assets/"):
     if len(contours) == 1:
         result = "Breaks"
     else:
-        # segmentin the inner circle to check if it has any defects
-        inv_thresh = cv2.bitwise_not(thresh)
-        contour_areas = [cv2.contourArea(cnt) for cnt in contours]
-        inner_contour = contours[np.argmin(contour_areas)].squeeze()
-        
-        # center points of the circle
-        cx = inner_contour[:, 0].mean().astype('int32')
-        cy = inner_contour[:, 1].mean().astype('int32')
+        for_harris = cv2.GaussianBlur(thresh, (3, 3), 2)
+        corners = harris_corner_detector(for_harris, k=0.2)
+        for x, y in corners:
+            cv2.circle(img, (x, y), 3, (0, 0, 255), cv2.FILLED)
 
     cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
 
-    # segmented = segment_objects(thresh).astype("uint8")
-    # segmented = colorize_labels(segmented)
-
-    cv2.imshow("window", stackIt([[img, thresh]], [[result, "thresholded"]]))
+    cv2.imshow("window", stackIt([[img, thresh, for_harris]], [[result, "thresholded"]]))
     if cv2.waitKey() & 0xFF == ord("q"):
         break
